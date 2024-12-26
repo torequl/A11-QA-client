54b35cae5a8160d4fa805157a3243eb5194844fe
torequl <torequlmkt@gmail.com>
2024-12-26 13:58:53 +0600

    Set and Delete token on the cookie

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -50,7 +50,28 @@ const AuthProvider = ({ children }) => {
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
             setUser(currentUser)
-            setLoading(false)
+            console.log(currentUser);
+
+            if(currentUser?.email){
+                const user = {email: currentUser.email};
+                axios.post('http://localhost:5000/jwt', user, {
+                    withCredentials: true,
+                })
+                .then(res => {
+                    console.log(res.data)
+                    setLoading(false)
+                })
+            }
+            else {
+                axios.post('http://localhost:5000/logout', {} ,{
+                    withCredentials: true,
+                })
+                .then(res => {
+                    console.log('logout', res.data)
+                    setLoading(false)
+                })
+            }
+
             return () => {
                 unsubscribe()
             }
